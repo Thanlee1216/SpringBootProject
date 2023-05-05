289dda708f9f68d1a760aa8ed0cbd0f56dc07e34
TanHan Lee <lth9312@gmail.com>
2023-05-05 22:25:58 +0900

    Init Spring Boot Project

---
+++ src/main/java/com/springboot/springbootprop/SpringbootpropApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootprop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootpropApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/springbootprop/SpringbootpropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootprop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpropApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
